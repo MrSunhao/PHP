5e737331f0c74bcdfd66b7094e06ee601688abf3
MrSunhao <1401243363@qq.com>
2017-06-06 08:30:15 +0800

    2017/6/6

--- sport/sport/Common/Conf/config.php
+++ sport/sport/Common/Conf/config.php
@@ -16,7 +16,7 @@ return array(
     'DB_HOST'=>'localhost',//服务器地址
     'DB_NAME'=>'runner',//数据库名字
     'DB_USER'=>'root',//用户名
-    'DB_PWD'=>'199648sth',//密码
+    'DB_PWD'=>'',//密码
     'DB_PORT'=>'3306',//端口
     'DB_PARAMS'=>array(),//数据库连接参数
     'DB_PREFIX'=>'',//数据库表前缀
